846b5baf4e3b307ed65699f7ce83268b61ca8be5
zhenkapechenka <zhenka_pechenka06@mail.ru>
2025-03-08 01:38:53 +0300

    класс add if min

--- src/main/java/com/manager/commands/AddIfMinCommand.java
+++ src/main/java/com/manager/commands/AddIfMinCommand.java
@@ -185,7 +185,89 @@ public class AddIfMinCommand extends Command {
                 }
                 updatedQueue.add(ticket);
                 found = true;
-            } else {
+            }
+            if (ticket.getCoordinates().getY() > Integer.parseInt(commandArgs[0]) && Integer.parseInt(commandArgs[0]) >= -133) {
+                int y = Integer.parseInt(commandArgs[0]);
+                try {
+                    System.out.println("Для нового билета введена координата по у " + Integer.parseInt(commandArgs[0]));
+
+                    System.out.print("Введите id билета: ");
+                    String id = scanner.nextLine();
+                    if (id == null || id.trim().isEmpty()) {
+                        throw new IllegalArgumentException("Id билета не может быть пустым.");
+                    }
+
+                    System.out.print("Введите имя билета: ");
+                    String name = scanner.nextLine();
+                    if (name == null || name.trim().isEmpty()) {
+                        throw new IllegalArgumentException("Имя билета не может быть пустым.");
+                    }
+
+                    System.out.print("Введите цену билета (больше 0): ");
+                    long price = Long.parseLong(scanner.nextLine());
+                    if (price <= 0) {
+                        throw new IllegalArgumentException("Цена должна быть больше 0.");
+                    }
+
+                    System.out.println("Введите координаты:");
+                    System.out.print("Введите x (больше -9): ");
+                    int x = Integer.parseInt(scanner.nextLine());
+                    if (x <= -9) {
+                        throw new IllegalArgumentException("Значение x должно быть больше -9.");
+                    }
+
+                    Coordinates coordinates = new Coordinates(x, y);
+
+                    TicketType type = null;
+                    boolean validType = false;
+                    while (!validType) {
+                        try {
+                            System.out.println("Выберите тип билета (VIP, USUAL, BUDGETARY, CHEAP):");
+                            String typeInput = scanner.nextLine().toUpperCase();
+                            type = TicketType.valueOf(typeInput);
+                            validType = true;
+                        } catch (IllegalArgumentException e) {
+                            System.err.println("Ошибка: неверный тип билета. Попробуйте снова.");
+                        }
+                    }
+
+
+                    System.out.print("Введите название места: ");
+                    String venueName = scanner.nextLine();
+                    if (venueName == null || venueName.trim().isEmpty()) {
+                        throw new IllegalArgumentException("Название места не может быть пустым.");
+                    }
+                    System.out.print("Введите вместимость места (может быть пустым): ");
+                    String capacityStr = scanner.nextLine();
+                    Integer capacity = (capacityStr.isEmpty()) ? null : Integer.parseInt(capacityStr);
+
+
+                    Venue venue = ticket.getVenue();
+                    venue.setName(venueName);
+                    venue.setCapacity(capacity);
+
+                    ticket.setName(name);
+                    ticket.setCoordinates(coordinates);
+                    ticket.setCreationDate(Date.from(Instant.now()));
+                    ticket.setPrice(price);
+                    ticket.setType(type);
+                    ticket.setVenue(venue);
+
+                    System.out.println("Билет успешно обновлён!");
+
+                } catch (NumberFormatException e) {
+                    System.err.println("Ошибка: введено неправильное число.");
+                } catch (IllegalArgumentException e) {
+                    System.err.println("Ошибка: " + e.getMessage());
+                } catch (Exception e) {
+                    System.err.println("Ошибка при добавлении билета: " + e.getMessage());
+                }
+                updatedQueue.add(ticket);
+                found = true;
+
+            }else {
+                System.err.println("Ошибка: параметр сравнения должен быть числом и подходить по параметрам.");
+
                 updatedQueue.add(ticket);
             }}
         env.setTickets(updatedQueue);
@@ -194,8 +276,9 @@ public class AddIfMinCommand extends Command {
             stdout.println("Билет был обновлён.");
         }
     } catch (NumberFormatException e) {
-        System.err.println("Ошибка: ID должен быть числом.");
+        System.err.println("Ошибка: параметр сравнения должен быть числом.");
     }
+
 }
 
 
